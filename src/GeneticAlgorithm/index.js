c76cad423c13dd73c80e16345ea7cc3a197bf685
sdemir <src.demir@gmail.com>
2023-05-06 00:22:35 +0300

    random path added

--- src/GeneticAlgorithm/index.js
+++ src/GeneticAlgorithm/index.js
@@ -1,4 +1,5 @@
-// import {findNearest} from "geolib";
+import {getDistance} from "geolib";
+import selection from "./selection_methods"
 
 class GeneticAlgorithm {
     constructor(iteration_size, population_size, selection_method, crossover_method, startEnd, cities) {
@@ -9,36 +10,51 @@ class GeneticAlgorithm {
         this.startEnd = startEnd;
         this.cities = cities;
         this.best = 0;
-        this.counter = 0
-        this.is_initialized = false;
-        this.path_deep = 5;
-        this.path = null;
+        this.population = [];
     }
 
-    random_path() {
-        let start_point = this.cities.find(x => x.plaka === 6)
-        let target_point = this.cities.find(x => x.plaka === 25)
+    random_path(start, end) {
+        let start_point = this.cities.find(x => x.plaka === start)
         let current_city = start_point;
         let path = [start_point]
-        for (let i = 0; i < this.path_deep; i++) {
-
+        for (let i = 0; i < this.cities.length - 1; i++) {
             while (path.some(x => x.plaka === current_city.plaka)) {
                 const random_neighbor = current_city.komsular.sort(() => 0.5 - Math.random())[0];
                 current_city = this.cities.find(x => x.plaka === random_neighbor)
             }
             path.push(current_city)
         }
+        let target_index = path.findIndex(x => x.plaka === end)
+        path = path.slice(0, target_index + 1)
+        this.fitness(path)
         return path
 
     }
 
+    fitness(path) {
+        let total_cost = 0;
+        for (let i = 0; i < path.length - 1; i++) {
+            total_cost += getDistance(
+                {latitude: path[i].latitude, longitude: path[i].longitude},
+                {latitude: path[i + 1].latitude, longitude: path[i + 1].longitude})
+        }
+        return total_cost
+    }
+
     initialize() {
-        this.path = this.random_path();
+        for (let i = 0; i < this.population_size; i++) {
+            let random_path = this.random_path(6, 25);
+            let cost = this.fitness(random_path)
+            this.population.push({
+                "path": random_path,
+                "cost": cost
+            });
+        }
     }
 
     start() {
         this.initialize();
-
+        selection(this.selection_method, this.population, 0.2)
         // setTimeout(() => {
         //     if (!this.is_initialized) {
         //         this.is_initialized = true;
@@ -47,5 +63,6 @@ class GeneticAlgorithm {
         // }, 1)
     }
 
-};
+}
+
 export default GeneticAlgorithm;
--- src/GeneticAlgorithm/selection_methods.js
+++ src/GeneticAlgorithm/selection_methods.js
@@ -1,16 +1,33 @@
-const roulette_wheel_selection = () => {
-
+const selection = (method, population, ratio) => {
+    switch (method) {
+        case 'roulette':
+            return roulette_wheel_selection(population, ratio);
+        case 'rank':
+            return rank_selection(population, ratio);
+        case 'tournament':
+            return tournament_selection(population, ratio);
+        case 'elitist':
+            return elitist_selection(population, ratio);
+        default:
+            return null;
+    }
+}
+const roulette_wheel_selection = (population, ratio) => {
+    population.map(x=>
+        console.log(x.cost)
+    )
 }
 
-const rank_selection = () => {
-
-
+const rank_selection = (population, ratio) => {
+    console.log("rank")
 }
 
-const tournament_selection = () => {
-
+const tournament_selection = (population, ratio) => {
+    console.log("tournament")
 }
 
-const elitist_selection = () => {
-
+const elitist_selection = (population, ratio) => {
+    console.log("elitist")
 }
+
+export default selection;
